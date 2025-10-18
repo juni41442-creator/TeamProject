a1a7f7efc9b1a3fecc981747740ee0e9aa035dc5
juni41442-creator <juni41442@gmail.com>
2025-10-18 17:37:13 +0900

    20251018 2D 파레트로 게임 제작

---
+++ TopDownUnityTeam01/Assets/Door.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //카메라가 잠시 어두워진다. 카메라 앞에 검은 색 종이를 배치.
+        //일정 시간 뒤에 밝아집니다.
+        //플레이어가 어떤 위치에서 등장합니다.
+
+        //CinnemachineConfiner2D -> BoundingShape A1 -> a1-1 (건물안의 경계선을 바꾼다.)
+        // 위치를 이동? 어디로 이동할지 기억 플레이어의 
+
+    }
+}
