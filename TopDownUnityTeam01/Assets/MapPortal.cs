730627d9b46c9ef39b08f8974513a3b51010410c
txt8750 <juni41442@gmail.com>
2025-10-25 17:39:26 +0900

    Add map portal system and panel transition animations
    Introduced MapPortal.cs for handling player transitions between map areas, including camera confiner and animation triggers. Added Animation assets (Show/Hide) and a Panel.controller for UI transitions. Updated Level0.unity to include portal entry/exit points, colliders, and references to the new portal system. Added new MapTransition and New Scene scenes for future use. Adjusted camera and pixel perfect settings for improved scene presentation.

---
+++ TopDownUnityTeam01/Assets/MapPortal.cs
@@ -0,0 +1,26 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MapPortal : MonoBehaviour
+{
+    //플레이어의 캐릭터를 원하는 지점으로 이동 해야 한다.
+
+    // Entry의 충돌 지점을 닿았을때
+    public Transform movePosition;
+    public Collider2D mapBoundry;
+    public CinemachineConfiner2D Confiner; //inemachineConfiner2D 데이터를 저장 할 수 있는 상자.
+    public Animator SceneTransitonAnimator; //방금 만들었던 씬 전환 애니메이터를 여기에 저장하세요.
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Confiner.BoundingShape2D = mapBoundry;
+            //Collision.transform.position = 이동하고 싶은 위치로 이동하라.
+            collision.transform.position = movePosition.position;
+
+            SceneTransitonAnimator.SetTrigger("Show");
+        }
+    }
+
+}
